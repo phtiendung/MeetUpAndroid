78b2ca83deb18c258011ef3ab81319d772a8bb27
manhld <manhmeu1211@gmail.com>
2020-01-07 09:15:54 +0700

    Databinding demo

--- app/src/main/java/com/example/meetup/Activity/HomeActivity.java
+++ app/src/main/java/com/example/meetup/Activity/HomeActivity.java
@@ -1,14 +1,18 @@
 package com.example.meetup.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+import com.example.meetup.R;
+import com.example.meetup.databinding.ActivityHomeBinding;
 
+public class HomeActivity extends AppCompatActivity {
+    ActivityHomeBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home);
     }
 }
